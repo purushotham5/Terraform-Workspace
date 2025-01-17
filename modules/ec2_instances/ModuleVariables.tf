c6cd97e289a7693b6aeadea40dfe3a8cb93899b4
Purushotham Kandheri <87255292+purushotham5@users.noreply.github.com>
2025-01-17 15:00:21 +0530

    Create ModuleVariables.tf

---
+++ modules/ec2_instances/ModuleVariables.tf
@@ -0,0 +1,11 @@
+variable "ami_value_Module" {
+  description = "ami value"
+}
+
+variable "instance_type_value_Module" {
+  description = "Instance Type Value"
+}
+
+variable "instance_name_value_Module" {
+  description = "name of the instance"
+}
